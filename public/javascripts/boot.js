b82c5cb8efa6186f90c8fe78587840c702b7f91b
Mark Suurmond <msuurmond@zilverline.com>
2012-08-22 14:03:29 +0200

    Added extra stages and extra workitems.

--- public/javascripts/boot.js
+++ public/javascripts/boot.js
@@ -1,14 +1,25 @@
 var createWorkItems = function () {
   return [
-    new WorkItem({name:"Feature #1", analysis:3, design:3, code:5, test:3, release:2}),
-    new WorkItem({name:"Feature #2", analysis:3, design:2, code:5, test:3, release:3})
+    new WorkItem({name:"Feature #1", analysis:2, design:2, code:4, test:2, release:1}),
+    new WorkItem({name:"Feature #2", analysis:2, design:2, code:4, test:3, release:1}),
+    new WorkItem({name:"Feature #3", analysis:3, design:2, code:6, test:3, release:2}),
+    new WorkItem({name:"Feature #4", analysis:3, design:2, code:5, test:2, release:2}),
+    new WorkItem({name:"Feature #5", analysis:3, design:2, code:6, test:4, release:2}),
+    new WorkItem({name:"Feature #6", analysis:3, design:1, code:4, test:2, release:1}),
+    new WorkItem({name:"Feature #7", analysis:2, design:2, code:4, test:2, release:2}),
+    new WorkItem({name:"Feature #8", analysis:3, design:1, code:5, test:2, release:2}),
+    new WorkItem({name:"Feature #9", analysis:2, design:0, code:3, test:2, release:1}),  
+    new WorkItem({name:"Feature #10", analysis:2, design:1, code:4, test:2, release:2})  
   ];
 };
 
 var createStages = function (backlog) {
   var analysis = new Stage({name:"Analysis", readyWorkProvider:backlog});
   var design = new Stage({name:"Design", readyWorkProvider:analysis});
-  return [analysis, design];
+  var code = new Stage({name:"Code", readyWorkProvider:design});
+  var test = new Stage({name:"Test", readyWorkProvider:code});
+  var release = new Stage({name:"Release", readyWorkProvider:test});
+  return [analysis, design, code, test, release];
 };
 
 $(function () {
