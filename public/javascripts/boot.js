ef46876c0551fe2eabecd2ddac8486e677078f6f
Bob Forma <bobforma@gmail.com>
2012-08-22 21:44:05 +0200

    cleanup

--- public/javascripts/boot.js
+++ public/javascripts/boot.js
@@ -1,23 +1,22 @@
-var createWorkItems = function () {
-  var workList = [];
-
-  for (var i = 0; i < 100; i++) {
-   workList[i] = new WorkItem({name:"Feature #"+i});  
-  }
-
-  return workList;
-};
-
-var createStages = function (backlog) {
-  var analysis = new Stage({name:"Analysis", readyWorkProvider:backlog});
-  var design = new Stage({name:"Design", readyWorkProvider:analysis});
-  var code = new Stage({name:"Code", readyWorkProvider:design});
-  var test = new Stage({name:"Test", readyWorkProvider:code});
-  var release = new Stage({name:"Release", readyWorkProvider:test});
-  return [analysis, design, code, test, release];
-};
-
 $(function () {
+  var createWorkItems = function () {
+    var workList = [];
+
+    for (var i = 0; i < 10; i++) {
+      workList[i] = new WorkItem({name:"Feature #"+i});
+    }
+
+    return workList;
+  };
+
+  var createStages = function (backlog) {
+    var analysis = new Stage({name:"Analysis", readyWorkProvider:backlog});
+    var design = new Stage({name:"Design", readyWorkProvider:analysis});
+    var code = new Stage({name:"Code", readyWorkProvider:design});
+    var test = new Stage({name:"Test", readyWorkProvider:code});
+    var release = new Stage({name:"Release", readyWorkProvider:test});
+    return [analysis, design, code, test, release];
+  };
 
   var startup = function () {
     var backlog = new ReadyWorkProvider({readyWork:createWorkItems()});
@@ -28,4 +27,5 @@ $(function () {
   };
 
   startup();
+  
 });
